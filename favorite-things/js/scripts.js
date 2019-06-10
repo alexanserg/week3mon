c4e11c1800c6149d63cf09598a5017c5b780a954
Alexander Sergejev and Ben Martinson <ben@>
2019-06-10 10:54:36 -0700

    change to a looping function to DRY code

--- favorite-things/js/scripts.js
+++ favorite-things/js/scripts.js
@@ -3,20 +3,14 @@ var favThings = [];
 $("document").ready(function() {
   $(".favorite-things").submit(function(event){
     event.preventDefault();
+    var favorites = ["bird", "food", "car", "city", "activity"]
 
+    favorites.forEach(function(favorite) {
+      var favVal = $("#"+favorite).val();
 
-    favThings.push($("#bird").val());
-    favThings.push($("#car").val());
-    favThings.push($("#food").val());
-    favThings.push($("#city").val());
-    favThings.push($("#activity").val());
-    console.log(favThings);
+      $("#listFav").append("<li>"+favVal+"</li>");
+    })
 
-    $("#listFav").append("<li>"+favThings[0]+"</li>")
-    $("#listFav").append("<li>"+favThings[1]+"</li>")
-    $("#listFav").append("<li>"+favThings[2]+"</li>")
-    $("#listFav").append("<li>"+favThings[3]+"</li>")
-    $("#listFav").append("<li>"+favThings[4]+"</li>")
   });
 
 })
